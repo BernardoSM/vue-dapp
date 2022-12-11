05a396f58730b09353228a15fe3fabe1570a372c
Bernardo Simonassi <bernardosimonassi@hotmail.com>
2022-12-11 15:39:01 -0300

    feat: add tailwind

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,10 @@
-<script setup>
-import TheWelcome from '../components/TheWelcome.vue'
-</script>
-
 <template>
-  <main>
-    <TheWelcome />
+  <main class="bg-gray-900 min-h-screen flex items-center">
+    <div
+      class="rounded-md border border-gray-700 bg-gray-800 p-6 mx-auto w-full max-w-[400px]"
+    >
+      <h1 class="text-2xl text-white">Vue dApp</h1>
+    </div>
+    <h1></h1>
   </main>
 </template>
